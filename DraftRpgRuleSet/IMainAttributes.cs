a9e519d33c66f5aa788d9997069ab8f07b961a49
Tony Bierman <tonybierman@gmail.com>
2022-12-28 15:13:08 -0600

    Main attributes

---
+++ DraftRpgRuleSet/IMainAttributes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftRpgRuleSet
+{
+    public interface IMainAttributes
+    {
+        #region Physical Attributes
+
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+
+        #endregion
+
+        #region Mental Attributes
+
+        public int Reason { get; set; }
+        public int Intuition { get; set; }
+        public int Willpower { get; set; }
+
+        #endregion
+
+        #region Interactive Attributes
+
+        public int Charisma { get; set; }
+        public int Perception { get; set; }
+        public int Empathy { get; set; }
+
+        #endregion
+
+    }
+}
