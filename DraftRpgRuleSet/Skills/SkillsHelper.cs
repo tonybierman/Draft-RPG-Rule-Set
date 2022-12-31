56db4c357a2099f9060a9bdb01fa95c4e719e87f
Tony Bierman <tonybierman@gmail.com>
2022-12-31 10:52:30 -0600

    Added command  and viewmodel interfaces

---
+++ DraftRpgRuleSet/Skills/SkillsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftRpgRuleSet.Skills
+{
+    public static class SkillsHelper
+    {
+        public static int SkillCheck()
+        {
+            /*
+             * Skill + Bonuses - Penalties + d10 > Difficulty
+             */
+
+            return 0;
+        }
+    }
+}
