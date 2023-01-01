1811667b7920daf5f1195482479bb682a5f5a13b
Tony Bierman <tonybierman@gmail.com>
2023-01-01 16:25:29 -0600

    Working on combat

---
+++ DraftRpgRuleSet/Combat/CombatPosture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftRpgRuleSet.Combat
+{
+    public enum CombatPosture
+    {
+        None = 0,
+        Attacker,
+        Defender
+    }
+}
